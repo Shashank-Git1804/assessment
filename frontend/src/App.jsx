2501897cc766d183e6d68e78af0300de95cb9719
Shashank P <shashank182004@gmail.com>
2026-04-23 21:08:53 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,60 +1,3 @@
-// import "./App.css";
-// import { createBrowserRouter, RouterProvider } from "react-router";
-// import Register from "./pages/Register";
-// import Login from "./pages/Login";
-// import AppLayout from "./Layout/AppLayout";
-// import MonitoringOfficer from "./pages/MonitoringOfficer";
-// import ProgramManager from "./pages/ProgramManager";
-// import Institution from "./pages/Institution";
-// import Trainer from "./pages/Trainer";
-// import Student from "./pages/Student";
-
-// function App() {
-//  return(
-
-//   <RouterProvider router={createBrowserRouter([
-//     {
-//       path:'/',
-//       element:<AppLayout/>,
-//       children:[
-//         {
-//           path:'/',
-//           element:<Login/>
-//         },
-//         {
-//           path:"register",
-//           element:<Register/>
-//         },
-//         {
-//           path:"trainer",
-//           element:<Trainer/>
-//         },
-//         {
-//           path:"student",
-//           element:<Student/>
-//         },
-//         {
-//           path:"institution",
-//           element:<Institution/>
-//         },
-//         {
-//           path:"programme-manager",
-//           element:<ProgramManager/>
-//         },
-//         {
-//           path:"monitoring-officer",
-//           element:<MonitoringOfficer/>
-
-//         }
-//       ]
-//     }
-//   ])}/>
-
-//  )
-// }
-
-// export default App;
-
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import { AuthProvider } from "./context/AuthContext";
@@ -92,4 +35,3 @@ function App() {
 }
 
 export default App;
-
